4e3080afea58d0f409827801cc30b35e0d036783
s00rajsnair <nairsooraj2000@gmail.com>
2022-01-05 01:21:39 +0530

    slight modifications to make this work for IIS Windows FTP Servers too

--- FTPeek.sh
+++ FTPeek.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# FTPeek
 # FTP ENUMERATION TOOL FOR FINDING 
 # COMMON MISCONFIGURATIONS AND VULNERABILITIES
 
@@ -33,104 +34,97 @@ function print_line(){
 # CHECK IF THE HOST IS UP 
 function check_host(){
 echo -e "Checking if the host is up ..."
-nmap $IP_ADDR -p $FTP_PORT &> /tmp/check_host.txt
+nmap $IP_ADDR -Pn &> /tmp/check_host.txt
 if grep -q "Host is up" /tmp/check_host.txt; 
 then
 HOST_UP=true
 fi
+rm  /tmp/check_host.txt
 }
 
 # CHECK IF THE FTP PORT $FTP_PORT IS OPEN 
 function  knock_port(){
 echo -e "Knocking the FTP port ..."
-if grep -q "$FTP_PORT/tcp open  ftp" /tmp/check_host.txt
+nmap $IP_ADDR -p $FTP_PORT -Pn &> /tmp/knock_port.txt
+if grep -q "$FTP_PORT/tcp open  ftp" /tmp/knock_port.txt;
 then
 PORT_OPEN=true
 fi 
+rm /tmp/knock_port.txt
 }
 
 # RETREIVING THE BANNER 
 function get_banner(){
 echo -e "Grabbing FTP Banner ..."
-nmap --script=banner $IP_ADDR -p $FTP_PORT > /tmp/banner.txt
-BANNER=$(grep "banner" /tmp/banner.txt)
+nmap --script=banner $IP_ADDR -p $FTP_PORT -Pn &> /tmp/get_banner.txt
+BANNER=$(grep "banner" /tmp/get_banner.txt)
 REMOVE_STRING="|_banner: "
 echo ${BANNER//$REMOVE_STRING}
+rm /tmp/get_banner.txt
 }
 
 # CHECK IF ANONYMOUS LOGIN IS ENABLED
 function check_anonymous(){
-nmap $IP_ADDR -p $FTP_PORT -A &> /tmp/check_anonymous.txt
+nmap $IP_ADDR -p $FTP_PORT -A -Pn &> /tmp/check_anonymous.txt
 echo "Checking for Anonymous Login ..."
 if grep -q "Anonymous FTP login allowed" /tmp/check_anonymous.txt
 then
 ANONYMOUS_LOGIN_ENABLED=true
 fi
+rm /tmp/check_anonymous.txt
 }
 
 # TRYING TO LOGIN AS ANONYMOUS USER
 function login_anonymous(){
-
 echo "Attempting Anonymous Login ..."
-bash -c "ftp -nv $IP_ADDR <<END
+bash -c "ftp -nvp $IP_ADDR <<END
 user anonymous anonymous
 bye
 END" &> /tmp/login_anonymous.txt
-if grep -q "Login successful" /tmp/login_anonymous.txt
+if egrep -q "Login successful|User logged in." /tmp/login_anonymous.txt 
 then
 ANONYMOUS_LOGIN_SUCCESSFUL=true
 fi
-
 rm /tmp/login_anonymous.txt
 }
 
 # LISING ALL THE FILES AND  DIRECTORIES
 function list_files_directories(){
-
 echo "Getting all the files and directories, including the hidden ones ..."
-
 wget -r ftp://$IP_ADDR &> /tmp/list_files_directories.txt 
-tree $IP_ADDR
+echo
+tree $IP_ADDR -a
 find $IP_ADDR -type f -name '.*' > /tmp/hidden_files.txt
 find $IP_ADDR -type d -name '.*' > /tmp/hidden_directories.txt
-
 HIDDEN_FILE_COUNT=$(cat /tmp/hidden_files.txt | wc -l)
 HIDDEN_DIRECTORY_COUNT=$(cat /tmp/hidden_directories.txt | wc -l)
-
+echo
 echo "Hidden Files : $HIDDEN_FILE_COUNT"
-echo "Hidden Directories : $HIDDEN_DIRECTORY_COUNT"
-echo 
-
+echo "Hidden Directories : $HIDDEN_DIRECTORY_COUNT"  
 if [ $HIDDEN_FILE_COUNT -gt 0 ]
 then
 echo "Hidden Files"
 echo "------------"
 cat /tmp/hidden_files.txt
 fi
-
 if [ $HIDDEN_DIRECTORY_COUNT -gt 0 ]
 then
 echo "Hidden Directories"
 echo "------------------"
 cat /tmp/hidden_directories.txt
 fi
-
 rm -rf $IP_ADDR
 rm /tmp/hidden_files.txt /tmp/hidden_directories.txt /tmp/list_files_directories.txt
-
 }
 
 # CHECK IF A FILE CAN BE UPLOADED TO THE HOST 
 function check_upload_permissions(){
-
 echo "Checking if anonymous upload is enabled ..."
 touch /tmp/check_upload.txt
-bash -c "ftp -nv $IP_ADDR <<END
+bash -c "ftp -nvp $IP_ADDR <<END
 user anonymous anonymous
 put /tmp/check_upload.txt check_upload.txt
 END" &> /tmp/check_upload_permissions.txt
-
-# cat /tmp/check_upload_permissions.txt
 if grep -q "Transfer complete" /tmp/check_upload_permissions.txt
 then
 UPLOAD_ENABLED=true
@@ -138,12 +132,10 @@ else
 UPLOAD_ENABLED=false
 fi
 rm /tmp/check_upload_permissions.txt /tmp/check_upload.txt
-
 }
 
-
 ################### DRIVER CODE ###################
-echo -e "[FTP ENUMERATION TOOL]"
+echo -e "[FTPeek]"
 echo
 
 check_host
@@ -154,7 +146,6 @@ else
 echo "$IP_ADDR is DOWN"
 fi
 echo
-
 
 knock_port
 if $PORT_OPEN 
@@ -197,7 +188,3 @@ else
 echo -e "File upload Failed"
 fi
 echo
-
-
-
-
